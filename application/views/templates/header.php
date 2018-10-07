6aaff3b3d149a3f142df3f8935553d21b4a6b643
yuntongzhang <ang.unong@gmail.com>
2018-10-07 19:40:31 +0800

    Enable Bootstrap

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,12 @@
 <html>
     <head>
         <title>CodeIgniter Tutorial</title>
+
+        <!-- Bootstrap CSS -->
+        <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap JS -->
+        <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+
     </head>
     <body>
 
