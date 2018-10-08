52c190c8803c50bd64be6a6c091c145f64580d1c
Aaron Ong <40322372+ongaaron96@users.noreply.github.com>
2018-10-08 19:28:39 +0800

    Add files via upload

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,7 @@
-        <em>&copy; 2015</em>
+        </div>
+        <div class="footer">
+            <p><a href="about">About</a></p>
+            <p><em>&copy; Car Rider, CS2102 Group 44, 2018 Sem 1</em></p>
+        </div>
     </body>
 </html>
